feff3f0d8f710fbdd32c34f73ed43ec14d3bfb3f
Piotr Filipecki <piotrekfilipecki@gmail.com>
2022-11-30 14:53:08 +0100

    video bg mobile1

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,11 @@ import styles from '../styles/Home.module.css'
 import {isMobile} from 'react-device-detect';
 
 const VideoTrigger = () => {
-  if(isMobile) {
-    return <source src="https://www.datocms-assets.com/26125/1669814018-bgmobile.mp4" type='video/mp4'/>
-  } else {
+  if(!isMobile) {
     return <source src="https://www.datocms-assets.com/26125/1669813896-bgdesktop.mp4" type='video/mp4'/>
+  } else {
+    return <source src="https://www.datocms-assets.com/26125/1669814018-bgmobile.mp4" type='video/mp4'/>
+    
   }
 }
 
@@ -26,7 +27,7 @@ export default function Home() {
         </video>
 
         <div className={styles.wrapper}>
-          <img src='https://www.datocms-assets.com/26125/1669814297-lxscover.png' />
+          <img className={styles.coverimage} src='https://www.datocms-assets.com/26125/1669814297-lxscover.png' />
           <a href="#">
             <img src="https://www.datocms-assets.com/26125/1669814626-orderbutton1-edited.png" />
           </a>
